3eb63d13f275c0966b3b409216e19bd9fd74c4b5
kevin cuadros <kevindavidcuadros@gmail.com>
2024-07-31 18:15:23 -0500

    Load images from FileManager, add cell image and label text

--- Project-1-3-milestone/ViewController.swift
+++ Project-1-3-milestone/ViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Image Shared"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        loadImages()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.image = UIImage(named: pictures[indexPath.row])
+        content.imageProperties.cornerRadius = 5
+        content.imageProperties.maximumSize = CGSize(width: 70, height: 30)
+        content.text = pictures[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func loadImages() {
+        let fm = FileManager.default
+        let path = Bundle.main.bundlePath
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasSuffix(".jpg"){
+                pictures.append(item)
+            }
+        }
     }
 
 
